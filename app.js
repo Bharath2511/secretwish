3f4b0b27a66918ad7f6ff84a74067277d2857d72
V S Bharath <bharathcse5t6@gmail.com>
2018-09-28 13:40:06 +0530

    hello world

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 var express     = require("express"),
     app         = express(),
     bodyParser  = require("body-parser"),
@@ -10,18 +11,23 @@ var express     = require("express"),
     LocalStrategy = require('passport-local')
     User        = require('./models/user'),
     methodOverride = require('method-override')
+    
 
 var requestRoutes = require('./routes/requests'),
     secretRoutes = require('./routes/secrets'),
     indexRoutes = require('./routes/index')
 
-mongoose.connect("mongodb://localhost/DeathWish");
+// const url = `mongodb://${process.env.DATABASE_USER}:${process.env.DATABASE_PASS}@ds117423.mlab.com:17423/deathwish
+const url = "mongodb://"+process.env.DATABASE_USER+":"+process.env.DATABASE_PASS+"@ds117423.mlab.com:17423/deathwish"
+
+mongoose.connect(url).catch((err) => {console.log(err)})
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride('_method'))
 app.use(flash())
 app.set("view engine", "ejs");
 app.use(express.static(__dirname+'/public'))
 //seedDB()
+console.log(process.env.DATABASE_USER)
 var requestRoutes = require('./routes/requests'),
     secretRoutes = require('./routes/secrets'),
     indexRoutes = require('./routes/index')
